902eeca8c1697f70389f6fc2f03401eafe7f8406
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2019-12-30 20:43:39 -0500

    most important commands

--- cheatsheet.sql
+++ cheatsheet.sql
@@ -1,1 +1,28 @@
+-- SQL Cheatsheet by Adam Jean-Laurent
+
+-- #################################################
+-- Most Important SQL Commands 
+
+-- Extracts data from a database
+SELECT 
+-- updates data in a database
+UPDATE
+-- deletes data from a database
+DELETE
+-- inserts new data into a database
+INSERT INTO 
+-- creates a new database 
+CREATE DATABASE 
+-- modifies a table
+ALTER TABLE
+-- deletes a table
+DROP TABLE
+-- creates an index(search key)
+CREATE INDEX
+-- deletes an index
+DROP INDEX
+-- #################################################
+
+-- Select All Records From A Table
+SELECT * FROM TableName;
 
